e89860e4eda5cc224fa2f8d4c2971bb93ee22514
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-15 14:31:22 +0530

    list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.set(0,"w");
+        Collections.addAll(list, "c","d","y","s","y");
+        list.remove("y");
+        String a=list.get(0);
+        System.out.println(a);
+        int ind=list.indexOf("w");
+        System.out.println(ind);
+        System.out.println(list.subList(0,2));
+        System.out.println(list);
     }
 }
