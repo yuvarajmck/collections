f62e060309e59711399af00ae291338adde94f6c
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-15 13:08:53 +0530

    Collections

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<String>();
-        list.add("a");
-        list.add("b");
-        for(String s:list){
-            System.out.println(s);
-        }
+        System.out.println("hello");
     }
 }
